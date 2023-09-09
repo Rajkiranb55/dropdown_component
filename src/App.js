ae0800a659432840a3801fefdc7b908c53f890d2
Rajkiranb55 <141542918+Rajkiranb55@users.noreply.github.com>
2023-09-09 13:29:59 +0530

    dont know

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Dropdown = () => {
+  const [isActive, setMouseHover] = useState(false);
+  const [isnotActive, setMouseRemoved] = useState(false);
+  const list = ["Delhi", "Hyderabad", "Mumbai", "Kerala"];
+  const [value, setValue] = useState("");
+
+  const displaySelected = (e) => {
+    // console.log(e.target.textContent);
+    setValue(e.target.textContent);
+    setMouseHover(false);
+    setMouseRemoved(true);
+  };
+  const dropdownListavailable = () => {
+    setMouseHover(true);
+  };
+
+  const results = list.map((result, index) => {
+    return (
+      <a href="#" key={index} onClick={displaySelected}>
+        {result}
+      </a>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Select Your State</h1>
+      <div className="dropdown">
+        <button
+          className="dropdown_button dropbtn "
+          onMouseEnter={dropdownListavailable}
         >
-          Learn React
-        </a>
-      </header>
+          I'm From
+        </button>
+        <div
+          className="dropdown-content"
+          style={{ display: `${isActive ? "block" : "none"}` }}
+        >
+          {results}
+        </div>
+      </div>
+      <div
+        className="stateStatement"
+        style={{ display: `${isnotActive ? "block" : "none"}` }}
+      >
+        I'm from {value}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Dropdown;
